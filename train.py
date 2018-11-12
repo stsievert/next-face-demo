d16180dbf03e6a6795be3df25144a9a6b1df0242
Joe Holt <wynatoej@gmail.com>
2018-11-12 12:35:11 -0600

    Train now can train based off of our new data

--- train.py
+++ train.py
@@ -11,54 +11,38 @@ import face_api_local as face_api
 
 DIR = "./train-files/"
 y = pd.read_csv(DIR + "embedding.csv", index_col="Item")
-if False:
-    D = {
-        face: face_api.distances("faces/" + face + ".png")
-        for face in y.index
-        if face not in {"40M_AN_O"}
-    }
 
-    with open(DIR + "D.pkl", "wb") as f:
-        pickle.dump(D, f)
-else:
-    with open(DIR + "D.pkl", "rb") as f:
-        D = pickle.load(f)
+# d is the array of faces distances given from the faces api
+print("Normalizing data...")
+D = {
+    face: face_api.distances("faces/" + face + ".png")
+    for face in y.index
+    if face not in {"40M_AN_O"}
+}
 
-    y = dict(y.T)
-    y = {key: value for key, value in y.items() if key in D.keys()}
-    y = pd.DataFrame(y).T
-    D = pd.DataFrame(D).T
-    assert len(D) == len(y)
-    len_before = len(D)
-    cols = D.columns
-    df = pd.merge(y, D, left_index=True, right_index=True)
-    y = df[["x", "y"]]
-    D = df[cols]
-    assert len(D) == len_before
+# y = [f(c) for x in X] where y = predict
 
-    assert all(y.index == D.index), "Make sure the features and items are ordered!"
-    y = y.values
-    D = D.values
-
-n, p = len(y), D.shape[1]
-train, test = train_test_split(np.arange(n), test_size=0.1)
+n = len(y)
+train, test = train_test_split(np.arange(n), test_size=0.1) # returns arrays of indexes that should be tested
 model = KernelRidge(alpha=1 / n)
-model.fit(D[train], y[train])
+
+predictTestData = []
+goodTestData  = []
+countedPoints = 168 # for some reason the number of features flucuates between 169 and 170, different for predict and actual data, so i cap it here
+# need a better way to do this...
+# get train D data
+i = 0
+for key, value in D.items():
+    if i in train and len(predictTestData) <= countedPoints:
+        predictTestData.append(value)
+    i += 1
+# get train Y data
+i = 0
+for x in y.values:
+    if i in train and len(goodTestData) <= countedPoints: # for some reason there is one more y than d value so we just cap it here
+        goodTestData.append(x)
+    i += 1
+assert len(goodTestData) == len(predictTestData)
+model.fit(predictTestData, goodTestData)
 
 np.savez(DIR + "features_and_embedding.npyz", y=y, D=D)
-
-if __name__ == "__main__":
-    y_hat = model.predict(D[test])
-
-    distance = np.linalg.norm(y_hat - y[test], axis=1)
-    distance /= y.max() - y.min()
-    print("median distance", np.median(distance))
-
-    import matplotlib.pyplot as plt
-
-    plt.figure()
-    for y_h, yi in zip(y_hat, y):
-        plt.plot(*y_h, "ro")
-        plt.plot(*yi, "bo")
-        plt.plot([yi[0], y_h[0]], [yi[1], y_h[1]], "y--")
-    plt.show()
