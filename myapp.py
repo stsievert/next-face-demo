71ecc5487a0dca205cc0abda9a42d2301501fb51
Joe Holt <wynatoej@gmail.com>
2019-02-19 13:02:25 -0600

    fixed color channel issues that would conflict either face_recognition or bokeh

--- myapp.py
+++ myapp.py
@@ -8,6 +8,7 @@ import imageio
 import numpy as np
 import get_words
 import plot_util
+from PIL import Image
 from bokeh.layouts import column
 from bokeh.models import Button, ColumnDataSource, CustomJS, Label, Text
 from bokeh.palettes import RdYlBu3
@@ -19,10 +20,15 @@ maxWebcamHeight = 240       # captured webcam image height
 FIGURE_DIM = (1200, 800)    # Set up size of figure
 plot = generate_initial_plot(test=True, n_imgs=50, img_width=0.3, dim=FIGURE_DIM)  # plot
 
-# take img and coords and update plot
-img = open("imgs/wanted.png", "rb")
+# had to open in PIL to get a ndarray representation rather than iobuffer while also loading in RGBA color space
+im = Image.open('imgs/wanted.png')
+im = im.convert("RGBA")
+imarray = np.array(im)
+imarray = np.flipud(imarray)
+
+# plot the image
 width = 0.3588 # 1.3 * 0.30 * 0.92
-e = plot.image_rgba(image=[img], x=[0], y=[0], dw=[width], dh=[width])
+e = plot.image_rgba(image=[imarray], x=[0], y=[0], dw=[width], dh=[width])
 
 # add the image label to the plot
 img_label = plot_util.make_image_label()
@@ -39,23 +45,23 @@ def callback(img_data):
     print("[MYAPP] Processing image callback")
     img_label.text = "Thinking..."
 
-    # img = imageio.imread(f.read())
-    if img_data.shape[-1] == 3:
-        print("changing color")
-        img_data = plot_util.change_color(img_data)
+    # what does this do?
     if min(img_data.shape[:2]) > 512:
         img_data = plot_util.reduce_image(img_data)
 
     try:
+        print("[MYAPP] Attempting prediction")
         y = predict(img_data)
     except FaceNotFoundException as exc:
         print("[MYAPP] Face not found, making random guess")
-        crop_image = False
         y = np.random.randn(2)
         y /= np.linalg.norm(y) * 2
 
     aspect_ratio = img_data.shape[0] / img_data.shape[1]
-    e.data_source.data.update(x = [y[0]], y = [y[1]], image = [img_data])
+
+    img_data_rgba = cv2.cvtColor(img_data, cv2.COLOR_RGB2RGBA)
+    img_data_rgba_flipped = np.flipud(img_data_rgba)
+    e.data_source.data.update(x = [y[0]], y = [y[1]], image = [img_data_rgba_flipped])
 
     emotions = get_words.find_words(y)
     print("[MYAPP] Predicted emotions: ", emotions)
@@ -75,9 +81,8 @@ def webcam_callback():
 
     print("[MYAPP] Reducing webcame image")
     imgSmall = plot_util.reduce_webcam_image(image, maxWebcamHeight)
-    imgSmallGray = cv2.cvtColor(imgSmall, cv2.COLOR_BGR2GRAY) # color channel not running correctly, use gray
-
-    callback(imgSmallGray)
+    imgRGB = cv2.cvtColor(imgSmall, cv2.COLOR_BGR2RGB)
+    callback(imgRGB)
 
 
 def test_callback():
@@ -91,10 +96,13 @@ def test_callback():
     print("[MYAPP][TEST] Processing image....")
     callback(im)
 
+def setup():
+    # put the button and plot in a layout and add to the document
+    take_picture_label = plot_util.make_take_picture_label()
+    take_picture_label.on_click(webcam_callback)
+    curdoc().add_root(column(take_picture_label, plot))
+    #webcam_callback()
 
-# put the button and plot in a layout and add to the document
-take_picture_label = plot_util.make_take_picture_label()
-take_picture_label.on_click(webcam_callback)
-curdoc().add_root(column(take_picture_label, plot))
+setup()
 
-test_callback()
+#test_callback()
