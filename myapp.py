523ba6fb6b3719f89965181a8600ef84b1d3242e
Joe Holt <wynatoej@gmail.com>
2019-03-12 16:30:51 -0500

    Camera now stops recording with stop webcam button

--- myapp.py
+++ myapp.py
@@ -2,10 +2,11 @@ import cv2
 import numpy as np
 import get_words
 import plot_util
-import threading
 import time
 import base64
 import io
+from functools import partial
+from threading import Thread
 from PIL import Image
 from bokeh import events
 from bokeh.layouts import column
@@ -16,6 +17,7 @@ from bokeh.palettes import RdYlBu3
 from bokeh.plotting import curdoc, figure
 from show_plot import generate_initial_plot, read_image
 from face_api_local import FaceNotFoundException, predict
+from tornado import gen
 
 # parameters ===================================================================
 
@@ -30,10 +32,16 @@ users.)
 """
 enable_cv2_capture = False
 
-maxWebcamHeight=240
+maxWebcamHeightCV2=240      # used for cv2
+image_capture_rate = 500.0    # take an image ever n milliseconds
+image_process_rate = 2000.0    # process image in python ever n seconds
+capture_height = 100         # these two are used for the general javascript capture
+capture_width = 200
 figure_size=(1200, 800)
 continue_loop = False;
 loop_duration = 0.1
+prev_image = ""             # previously processed image
+doc = curdoc()
 
 if enable_cv2_capture:
     take_picture_label = plot_util.make_take_picture_label()
@@ -44,15 +52,15 @@ base64_label = plot_util.make_image_base_input()
 
 # javascript ===================================================================
 
-prime_javascript_webcam = CustomJS(args=dict(label=base64_label), code="""
+prime_javascript_webcam = CustomJS(args=dict(label=base64_label, process=process_webcam_label, height=capture_height, width=capture_width, time=image_capture_rate), code="""
 
     // canvas and context set up
     var canvas = document.createElement('CANVAS');
     document.body.appendChild(canvas);
     canvas.style.visibility = "hidden";
     const context = canvas.getContext('2d');
-    context.canvas.width = 200;  // set to smaller size to reduce web trafic
-    context.canvas.height = 100; // set to smaller size to reduce web trafic
+    context.canvas.width = width;  // set to smaller size to reduce web trafic
+    context.canvas.height = height; // set to smaller size to reduce web trafic
     // video player
     var player = document.createElement('video');
     player.autoplay = true;
@@ -66,16 +74,41 @@ prime_javascript_webcam = CustomJS(args=dict(label=base64_label), code="""
     document.body.appendChild(saveButton);
     let image;
 
+    // if not there, add a false loop variable
+    if (localStorage.getItem('loop') === null) {
+        localStorage.setItem('loop', 'true');
+    } else if (localStorage.getItem('loop') === 'true') {
+        localStorage.setItem('loop', 'false');
+    } else {
+        localStorage.setItem('loop', 'true');
+    }
+
     saveButton.addEventListener('click', () => {
         context.drawImage(player, 0, 0, canvas.width, canvas.height);
         image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
     });
 
-    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-        player.srcObject = stream;
-        setTimeout(function() { saveButton.click();label.value = image; }, 2000);
-    });
+    if (localStorage.getItem('loop') === 'true') {
+        navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+            var interval = setInterval(function() {
+                player.srcObject = stream;
+                setTimeout(function() {
+                    saveButton.click();
+                    label.value = image;
+                    console.log("Capturing Image");
+                    // use this to see if the loop should continue
+                    var cnt = localStorage.getItem('loop');
+                    if (cnt == 'false') {
+                        clearInterval(interval);
+                    }
+                }, 2000);
+            }, time);
+        });
+    }
+""")
 
+end_javascript_webcam = CustomJS(args=dict(), code="""
+    window.localStorage.setItem('loop', false);
 """)
 
 # general set up ===============================================================
@@ -112,7 +145,7 @@ def callback(img_data):
     img_label.text = "Thinking..."
 
     print("[MYAPP] Processing image")
-    img_rgb, img_rgba = plot_util.process_image(img_data, maxWebcamHeight)
+    img_rgb, img_rgba = plot_util.process_image(img_data, maxWebcamHeightCV2)
 
     print("[MYAPP] Attempting prediction")
     try:
@@ -131,6 +164,38 @@ def callback(img_data):
     img_label.y = y[1]
     img_label.text = ", ".join(emotions)
     print("[MYAPP] Predicted emotions: ", emotions)
+
+def process_base64_image():
+    '''
+    processes javascript image in the base64 label
+    '''
+    print("[MYAPP] Capturing image via javascript webcam")
+
+    global prev_image
+    fullBase64 = base64_label.value
+    trimmedBase64 = fullBase64[len("data:image/octet-stream;base64,"):]
+    if trimmedBase64 != prev_image:
+        prev_image = trimmedBase64
+        print("[MYAPP] Setting Base64 Image of Length: " + str(len(trimmedBase64)))
+        imgdata = base64.b64decode(trimmedBase64)
+        image = Image.open(io.BytesIO(imgdata))
+        numpImage = np.asarray(image)
+        numpImage = cv2.cvtColor(numpImage, cv2.COLOR_BGR2RGB)
+        callback(numpImage)
+    else:
+        print("[MYAPP] Image not changed, not further work being done")
+
+def prime_webcam_clicked():
+    """ primes a the webcam python call back, change button """
+    if prime_webcam_label.button_type == "danger":
+        prime_webcam_label.label = "Start"
+        prime_webcam_label.button_type = "success"
+        process_webcam_label.disabled = True
+    else:
+        prime_webcam_label.label = "Stop Webcam"
+        prime_webcam_label.button_type = "danger"
+        process_webcam_label.disabled = False
+
 
 def server_webcam_callback(camera=None, delete_camera=True):
     '''
@@ -154,21 +219,6 @@ def server_webcam_callback(camera=None, delete_camera=True):
         del(camera)
 
     callback(image)
-
-def javascript_webcam_callback(activateWebcam=False):
-    '''
-    javascript implementation of javascript webcam funcitonality
-    '''
-    print("[MYAPP] Capturing image via javascript webcam")
-
-    fullBase64 = base64_label.value
-    trimmedBase64 = fullBase64[len("data:image/octet-stream;base64,"):]
-    print("[JAVASCRIPT CALLBACK] Image Base 64 Len: " + str(len(trimmedBase64)))
-    imgdata = base64.b64decode(trimmedBase64)
-    image = Image.open(io.BytesIO(imgdata))
-    numpImage = np.asarray(image)
-    numpImage = cv2.cvtColor(numpImage, cv2.COLOR_BGR2RGB)
-    callback(numpImage)
 
 def test_callback():
     """
@@ -221,11 +271,15 @@ def setup():
         take_picture_label.on_click(server_webcam_callback)
         picture_stream_label.on_click(toggle_picture_stream)
     prime_webcam_label.js_on_event(events.ButtonClick, prime_javascript_webcam)
-    process_webcam_label.on_click(javascript_webcam_callback)
+    prime_webcam_label.on_click(prime_webcam_clicked)
+    process_webcam_label.on_click(process_base64_image)
+    process_webcam_label.disabled = True
     if enable_cv2_capture:
         curdoc().add_root(column(take_picture_label, picture_stream_label, prime_webcam_label, process_webcam_label, base64_label, plot))
     else:
-        curdoc().add_root(column(prime_webcam_label, process_webcam_label, base64_label, plot))
+        curdoc().add_root(column(prime_webcam_label, process_webcam_label, plot))
 
 setup()
+#thread = Thread(target=begin_webcam_process_cycle)
+#thread.start()
 #test_callback()
